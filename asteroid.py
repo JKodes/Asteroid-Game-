5b41cb53f07fc6dc3d33262820cb9249391d97d0
unknown <jawanistanford@students.ptechnyc.org>
2022-08-10 15:10:22 -0400

    reset the entire file

--- asteroid.py
+++ asteroid.py
@@ -1,27 +1,5 @@
 import pygame
 from sys import exit 
 
-pygame.init()
-
-screen = pygame.display.set_mode((1270,800))
-pygame.display.set_caption('Asteroids')
-mainclock = pygame.time.Clock()
-
-bg_image = pygame.image.load('images/asteriod_galaxy.png')
 
 
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-
-
-    screen.blit(bg_image,(0,0))
-
-
-
-
-    pygame.display.update()
-    mainclock.tick(60)
