427f36404490c3e923dbad21cf61cb57e788ad3b
unknown <jawanistanford@students.ptechnyc.org>
2022-08-12 20:07:13 -0400

    create  main game loop

--- asteroid.py
+++ asteroid.py
@@ -9,5 +9,17 @@ display_surface = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Asteriod")
 
 
+run = True 
 
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run == False
+
+
+
+
+
+pygame.quit()
 
