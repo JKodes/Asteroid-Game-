2ebc7204b3b799db0c7000e82f9d60be788a8ae3
unknown <jawanistanford@students.ptechnyc.org>
2022-08-13 20:02:22 -0400

    add rect around image for future collision and boundaries

--- asteroid.py
+++ asteroid.py
@@ -2,6 +2,7 @@ from turtle import window_height, window_width
 import pygame
 import os
 
+
 pygame.init()
 
 window_width = 1000
@@ -15,9 +16,17 @@ default_black  = (105, 105, 105)
 fps = 60
 clock = pygame.time.Clock()
 
+vel = 5
+
+rocket= pygame.image.load(os.path.join('Assets', 'station_rocket.png')).convert_alpha()
+rocket_rect = rocket.get_rect()
+rocket_rect.centerx = window_width// 2
+rocket_rect.bottom = window_height//2
 
 
-rocket= pygame.image.load(os.path.join('Assets', 'station_rocket.png'))
+game_background = pygame.image.load(os.path.join('Assets', 'asteroid_bg.jpg'))
+
+
 
 run = True 
 
@@ -26,11 +35,15 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    
     
     display_surface.fill(default_black)
+    display_surface.blit(game_background, (0, 0))
 
-
-    display_surface.blit(rocket, (200, 100))
+    
+    display_surface.blit(rocket, rocket_rect)
+    
 
 
 
