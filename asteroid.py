d85db74bbd68e35c1738b08963b63f083ebb7333
unknown <jawanistanford@students.ptechnyc.org>
2022-09-12 09:10:41 -0400

    controls and everything at this point are not capable of doing anything right at this point. think of revamping the project an using OOP to improve that part of Python skills.

--- asteroid.py
+++ asteroid.py
@@ -1,3 +1,4 @@
+
 from turtle import window_height, window_width
 import pygame
 import os
@@ -10,21 +11,21 @@ window_height = 700
 display_surface = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Asteriod")
 
-
-default_black  = (105, 105, 105)
+default_black =(105, 105, 105)
 
 fps = 60
 clock = pygame.time.Clock()
 
-vel = 15
+vel = 5
 
 rocket = pygame.image.load(os.path.join('Assets', 'station_rocket.png')).convert_alpha()
 rocket_rect = rocket.get_rect()
-rocket_rect.center = window_width// 2 , window_height//2
+rocket_rect.center = pygame.math.Vector2((window_width//2, window_height//2))
+direction = pygame.math.Vector2(5, 0)
 
 rocket_move =[pygame.image.load(os.path.join('Assets', 'moving_rocket.png'))]
 
-game_background = pygame.image.load(os.path.join('Assets', 'asteroid_bg.jpg'))
+game_background = pygame.transform.scale(pygame.image.load('Assets/asteroid_bg.jpg'), (window_width, window_height))
 
 
 
@@ -38,17 +39,21 @@ while run:
         
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and rocket_rect.left > 0:
-        rocket_rect.x -= vel
-    if keys[pygame.K_RIGHT] and rocket_rect.right < window_width:
-        rocket_rect.x += vel
-    if keys[pygame.K_UP] and rocket_rect.top > 0:
-        rocket_rect.y -= vel
-    if keys[pygame.K_DOWN] and rocket_rect.bottom < window_height:
-        rocket_rect.y += vel
+    if keys[pygame.K_LEFT]:
+        direction.rotate_ip(-1)
+    if keys[pygame.K_RIGHT]:
+        direction.rotate_ip(1)
+    if keys[pygame.K_UP]:
+        rocket_rect.center += direction   
+    if keys[pygame.K_DOWN]:
+        rocket_rect.center -= direction
+
+    angle = direction.angle_to((1, 0))
+    rotated_car = pygame.transform.rotate(rocket, angle)
+    display_surface.blit(rotated_car, rotated_car.get_rect(center = (round(rocket_rect.center), round(rocket_rect.center.y))))
     
     
-    display_surface.fill(default_black)
+    display_surface.fill((default_black))
     display_surface.blit(game_background, (0, 0))
 
     
