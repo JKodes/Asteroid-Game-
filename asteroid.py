39df2d4fed59116e2a50ac18f309b1d9ae63d477
JKodes <adrainford09@gmail.com>
2022-10-17 10:34:38 -0400

    added background

--- asteroid.py
+++ asteroid.py
@@ -1,12 +1,21 @@
 import pygame
+import os
 
 pygame.init()
 
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 600
 
+
+game_bg = pygame.image.load(os.path.join('Assets', 'asteroid_bg.jpg'))
+
 display_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Asteriod")
+
+
+
+
+
 
 FPS = 60
 clock = pygame.time.Clock()
@@ -17,7 +26,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    display_window.fill((0, 0, 0))
+    display_window.fill((0,0,0))
+    display_window.blit(game_bg,(0,0))
     pygame.display.update()
     clock.tick(FPS)
 
