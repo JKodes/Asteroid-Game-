18172f286b7efcbdca1c386160e565ef02c41bb3
unknown <jawanistanford@students.ptechnyc.org>
2023-03-31 20:22:43 -0400

    new beginnings

--- asteroid.py
+++ asteroid.py
@@ -1,125 +1,23 @@
-import pygame
-import os
+import pygame, sys
 
 
-#Initialize Pygame
 pygame.init()
 
-
-#Set display window and Caption
 WINDOW_WIDTH = 1000
-WINDOW_HEIGHT = 600
-display_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+WINDOW_HEIGHT = 500
+
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Asteriod")
 
-#background for the game
-game_bg = pygame.image.load(os.path.join("Assets", "asteroid_bg.jpg"))
-
-#Set FPS and Clock
-FPS = 60
 clock = pygame.time.Clock()
 
-
-class Game():
-    """A class take oversee the enitre game"""
-    def __init__(self):
-        """Intilize the game object"""
-        self.score = 0
-
-        #Font
-        self.font = pygame.font.Font(os.path.join("Assets", "AnkhSanctuary-nROx4.ttf", 24))
-
-
-
-
-
-    
-
-    def update(self):
-        """Update our game object"""
-        pass
-    def draw(self):
-        """Draw the HUD and other to the display"""
-        #Color Set
-        WHITE = (255, 255,255)
-
-
-        score_text = self.font.render("Score: " + str(self.score), True, WHITE)
-        score_rect = score_text.get_rect()
-        score_rect.topleft()
-
-        life_text = self.font.render("Lives: " + str(self.player.lives), True, WHITE)
-        life_rect = life_text.get_rect()
-        life_rect.topleft(5,20)
-
-
-
-        #Blit 
-        display_window.blit(score_text, score_rect)
-    
-    def check_for_collisions(self):
-        """Check for collision between player and asteriod"""
-        pass
-
-    def pause_the_game(self):
-        pass
-
-    def reset_game(self):
-        pass
-
-
-class Player(pygame.sprite.Sprite):
-    '''A player class that the user can control'''
-    def __init__(self):
-        """Initailize the Player"""
-        super().__init__()
-        self.image = pygame.image.load(os.path.join('Assets', 'station_rocket.png'))
-        self.rect = self.image.get_rect()
-        self.rect.centerx = WINDOW_WIDTH//2
-
-
-        self.lives = 3
-        self.velocity = 7
-
-        self.hit_sound = pygame.mixer.Sound("hit.wav")
-        self.crash = pygame.mixer.Sound("")
-
-    def update(self):
-
-        keys = pygame.key.get_pressed()
-        moved = False
-        pass
-
-    def shoot(self):
-        pass
-
-    def reset(self):
-        self.rect.centerx = WINDOW_WIDTH//2
-        
-
-#Create game object
-my_game = Game()
-
-#main game loop
-running = True
-while running:
-    #Check to see if user wants to quit
+run = True
+while run: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
 
-    #Fill in the game an add background
-    display_window.fill((0,0,0))
-    display_window.blit(game_bg,(0,0))
+    dt = clock.tick() / 1000
 
-
-    
-
-    #Update display and tick clock
     pygame.display.update()
-    clock.tick(FPS)
-
-
-
-#End game 
-pygame.quit()
