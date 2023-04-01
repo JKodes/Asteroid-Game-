b0b822d3d6fc6c9e4a1d1af7605b35b0c2403677
unknown <jawanistanford@students.ptechnyc.org>
2023-03-31 23:42:13 -0400

    new class for ship

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,14 @@
 import pygame, sys
 
 
+class RocketShip(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super.__init__(groups)
+
+        self.image = pygame.image.load("/Assets/station_rocket.png").convert.alpha()
+        self.rect = self.image.get_rect(center = (WINDOW_WIDTH /2, WINDOW_HEIGHT /2))
+
+
 pygame.init()
 
 WINDOW_WIDTH = 1000
@@ -10,6 +18,12 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Asteriod")
 
 clock = pygame.time.Clock()
+
+
+rocket_group =pygame.sprite.Group()
+
+
+space_craft = RocketShip(rocket_group)
 
 run = True
 while run: 
