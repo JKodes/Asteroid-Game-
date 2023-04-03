22a97d15f13dba3cab71bbd9b6e0960aca2c4649
unknown <jawanistanford@students.ptechnyc.org>
2023-04-03 10:27:27 -0400

    function to rotate an image

--- utility.py
+++ utility.py
@@ -1,8 +1,5 @@
 import pygame
 
-def scale_image(img, factor):
-    size = round(img.get_width() * factor), round(img.get_height() * factor)
-    return pygame.transform.scale(img, size)
 
 def blit_rotate_center(display, image, top_left, angle):
     rotated_image = pygame.transform.rotate(image, angle)
