6d73d80a3263c52ef037fc27e738f85a2f91b0f9
unknown <jawanistanford@students.ptechnyc.org>
2023-04-03 20:14:04 -0400

    rocket moves directional

--- asteroid.py
+++ asteroid.py
@@ -1,13 +1,13 @@
-import pygame, sys, os
+import pygame, sys, os, math
 from utility import blit_rotate_center
-
 
 class Rocket(pygame.sprite.Sprite):
     def __init__(self, max_vel, rotate_vel, groups):
         super().__init__(groups)
+        pygame.image.get_extended()  # Disable cache for all images loaded afterwards
         self.image = pygame.image.load(os.path.join('Assets/images', 'betterquality_05.png'))
-        self.rect = self.image.get_rect(center = (WINDOW_WIDTH /2, WINDOW_HEIGHT /2))
-        self.max_vel = max_vel      #line 8 - 13 new code dont know if it work
+        self.rect = self.image.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.max_vel = max_vel
         self.vel = 0
         self.rotate_vel = rotate_vel
         self.angle = 0
@@ -20,25 +20,28 @@ class Rocket(pygame.sprite.Sprite):
         elif right:
             self.angle -= self.rotate_vel
 
-
     def move_forward(self):
         self.vel = min(self.vel + self.acceleration, self.max_vel)
         self.move()
 
+    def move(self):
+        radians = math.radians(self.angle)
+        vertical_vel = math.cos(radians) * self.vel
+        horizontal_vel = math.sin(radians) * self.vel
 
+        self.y -= vertical_vel
+        self.x -= horizontal_vel
 
-
-
-
-
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration / 2, 0)
+        self.move()
 
 class Bullets(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
+        pygame.image.get_extended()  # Disable cache for all images loaded afterwards
         self.image = pygame.image.load(os.path.join('Assets/images', 'bullets_03.png'))
-        self.rect = self.image.get_rect(midbottom = pos)
-
-
+        self.rect = self.image.get_rect(midbottom=pos)
 
 pygame.init()
 
@@ -49,20 +52,16 @@ pygame.display.set_caption("Asteriod")
 
 clock = pygame.time.Clock()
 
-#background = pygame.image.load("Assets/images/asteriod_galaxy.png").convert_alpha()  #this line needs to be fixed
 background = pygame.image.load(os.path.join('Assets/images', 'bryan-goff-f7YQo-eYHdM-unsplash.jpg'))
-
 
 rocket_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
 
-
-rocket = Rocket(rocket_group, 4,4)  
-bullet = Bullets((50,100),bullet_group)
-
+rocket = Rocket(4, 4, rocket_group)
+bullet = Bullets((50, 100), bullet_group)
 
 run = True
-while run: 
+while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -70,17 +69,23 @@ while run:
 
     dt = clock.tick() / 1000
 
-    display_surface.blit(background,(0,0))
+    display_surface.blit(background, (0, 0))
 
-    blit_rotate_center(display, self.image, (self.x, self.y), self.angle)
-
+    blit_rotate_center(display_surface, rocket.image, (rocket.x, rocket.y), rocket.angle)
 
     keys = pygame.key.get_pressed()
-    
-    if [pygame.K_LEFT]:
+    moved = False
+
+    if keys[pygame.K_LEFT]:
         rocket.rotate(left=True)
-    if [pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT]:
         rocket.rotate(right=True)
+    if keys[pygame.K_UP]:
+        moved = True
+        rocket.move_forward()
+
+    if not moved:
+        rocket.reduce_speed()
 
     rocket_group.draw(display_surface)
     bullet_group.draw(display_surface)
